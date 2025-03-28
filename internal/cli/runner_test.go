3d02004bd561137b6cc9795afcddb55ed93f93a6
Tom <tom.medhurst@vigilantapps.com>
2025-03-28 11:06:31 +0000

    improve code coverage testing error states

--- internal/cli/runner_test.go
+++ internal/cli/runner_test.go
@@ -7,16 +7,90 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_RunCLI_Simplest(t *testing.T) {
-	var in bytes.Buffer
-	var out bytes.Buffer
-	var err bytes.Buffer
+func Test_RunCLI(t *testing.T) {
+	const goodInput = `{"foo":"bar"}`
+	type args struct {
+		input string
+		args  []string
+	}
+	tests := []struct {
+		name            string
+		args            args
+		want            int
+		wantContains    string
+		wantErrContains string
+	}{
+		{
+			name: "simplest",
+			args: args{
+				input: goodInput,
+				args:  []string{"../../examples/simplest.json"},
+			},
+			want:         0,
+			wantContains: `"foo": "bar"`,
+		},
+		{
+			name: "no instructions file",
+			args: args{
+				input: goodInput,
+				args:  []string{},
+			},
+			want:            1,
+			wantErrContains: "usage:",
+		},
+		{
+			name: "invalid instructions file",
+			args: args{
+				input: goodInput,
+				args:  []string{"./does-not-exist.json"},
+			},
+			want:            1,
+			wantErrContains: "failed to read program file",
+		},
+		{
+			name: "invalid json instructions file",
+			args: args{
+				input: goodInput,
+				args:  []string{"../../invalid-json.json"},
+			},
+			want:            1,
+			wantErrContains: "failed to read program file",
+		},
+		{
+			name: "invalid json input piped in",
+			args: args{
+				input: `{`,
+				args:  []string{"../../examples/simplest.json"},
+			},
+			want:            1,
+			wantErrContains: "failed to parse input json",
+		},
+		{
+			name: "invalid op",
+			args: args{
+				input: goodInput,
+				args:  []string{"../../test/data/invalid-op.json"},
+			},
+			want:            1,
+			wantErrContains: "error:",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var in bytes.Buffer
+			var out bytes.Buffer
+			var err bytes.Buffer
 
-	// Prepare input
-	in.WriteString(`{"foo":"bar"}`)
+			// Assemble
+			in.WriteString(tt.args.input)
 
-	code := RunCLI([]string{"../../examples/simplest.json"}, &in, &out, &err)
+			// Act
+			code := RunCLI(tt.args.args, &in, &out, &err)
 
-	assert.Equal(t, 0, code)
-	assert.Contains(t, out.String(), `"foo": "bar"`)
+			// Assert
+			assert.Equal(t, tt.want, code)
+			assert.Contains(t, out.String(), tt.wantContains)
+			assert.Contains(t, err.String(), tt.wantErrContains)
+		})
+	}
 }
