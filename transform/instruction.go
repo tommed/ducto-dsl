41533e1651439353277082e196b3668c63a15698
tommed <tommed@users.noreply.github.com>
2025-04-04 14:28:56 +0000

    deploy: bc4bbfa8eca15b1337fa92f103b99df3af7a9498

--- transform/instruction.go
+++ transform/instruction.go
@@ -1,30 +1,28 @@
 package transform
 
 type Program struct {
-	Version      int           `json:"version"`
-	OnError      string        `json:"on_error,omitempty"`
-	Instructions []Instruction `json:"instructions"`
+	Version      int           `json:"version" mapstructure:"version"`
+	OnError      string        `json:"on_error,omitempty" mapstructure:"on_error"`
+	Instructions []Instruction `json:"instructions" mapstructure:"instructions"`
 }
 
 type Instruction struct {
-	Op string `json:"op"`
+	Op string `json:"op" mapstructure:"op"`
 
 	// Common fields
-	Key   string      `json:"key,omitempty"`
-	Value interface{} `json:"value,omitempty"`
+	Key   string      `json:"key,omitempty" mapstructure:"key"`
+	Value interface{} `json:"value,omitempty" mapstructure:"value"`
 
 	// Copy
-	From string `json:"from,omitempty"`
-	To   string `json:"to,omitempty"`
+	From string `json:"from,omitempty" mapstructure:"from"`
+	To   string `json:"to,omitempty" mapstructure:"to"`
 
 	// Delete
-	Regex bool `json:"regex,omitempty"`
+	Regex bool `json:"regex,omitempty" mapstructure:"regex"`
 
 	// Conditional
-	Condition map[string]interface{} `json:"condition,omitempty"`
-	Not       bool                   `json:"not,omitempty"`
-	Then      []Instruction          `json:"then,omitempty"`
-	IfNotSet  bool                   `json:"if_not_set,omitempty"`
-
-	// Others will be added as needed
+	Condition map[string]interface{} `json:"condition,omitempty" mapstructure:"condition"`
+	Not       bool                   `json:"not,omitempty" mapstructure:"not"`
+	Then      []Instruction          `json:"then,omitempty" mapstructure:"then"`
+	IfNotSet  bool                   `json:"if_not_set,omitempty" mapstructure:"if_not_set"`
 }
